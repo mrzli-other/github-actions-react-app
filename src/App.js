80b7c21b7f83c5b4494c321cd275abd519f3f06d
Goran Mržljak <goran.mrzljak@gmail.com>
2021-06-19 14:14:22 +0200

    feat(app): change text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 4
+          Learn Reac 4
         </a>
       </header>
     </div>
