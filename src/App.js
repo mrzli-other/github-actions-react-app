9dde7d4584b047b987cec32d7cb6902c2be05338
Goran Mržljak <goran.mrzljak@gmail.com>
2021-06-19 15:06:25 +0200

    fix(app): fix failing test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reac 4
+          Learn React 4
         </a>
       </header>
     </div>
