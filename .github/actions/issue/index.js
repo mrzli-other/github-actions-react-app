9db4d3f1ef8d4197bf9625d00136ef2f6623e5b4
Goran Mržljak <goran.mrzljak@gmail.com>
2021-06-19 18:22:55 +0200

    issue action

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,24 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+// before committing changes in here, run:
+// npx ncc build .github/actions/issue/index.js -o .github/actions/issue/dist
+try {
+  const token = core.getInput("token");
+  const title = core.getInput("title");
+  const body = core.getInput("body");
+  const assignees = core.getInput("assignees");
+
+  const octokit = new github.GitHub(token);
+  const response = octokit.issues.create({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    title,
+    body,
+    assignees: assignees ? assignees.split(",") : undefined
+  });
+
+  core.setOutput("issue", JSON.stringify(response.data));
+} catch (error) {
+  core.setFailed(error.message);
+}
